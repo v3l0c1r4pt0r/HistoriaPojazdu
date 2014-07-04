a542b2b6bde55da0815a071887fda09d574a286c
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-07-04 13:48:40 +0200

    renamed results activity

--- src/tk/v3l0c1r4pt0r/cepik/ResultActivity.java
+++ src/tk/v3l0c1r4pt0r/cepik/ResultActivity.java
@@ -46,7 +46,7 @@ public class ResultActivity extends ActionBarActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_test);
+		setContentView(R.layout.activity_results);
 		
 		//pobierz wartość z poprzedniej aktywności
 		Intent intent = getIntent();
